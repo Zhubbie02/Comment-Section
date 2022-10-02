a9184d16e1a3779795a1daa888daf2fe66a2ce53
Zhubbie02 <nzubeokwuoha@gmail.com>
2022-10-02 11:47:48 +0100

    fourth commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [comment, setComment]=useState("");
+  const [comments, setComments]=useState([]);
+
+  const onClickHandler = () => {
+    setComments((comments) => [...comments, comment]
+  )};
+
+  const onChangeHandler = (e) => {
+    setComment(e.target.value)
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="comment-section">
+      {comments.map((text) => (
+        <div className="comment-display">{text}</div>
+      ))}
+      <div className="comment-container">
+        <h1 className='title'>Comments</h1>
+        <textarea value={comment} onChange={onChangeHandler} placeholder="Please drop a comment..." className="input"/>
+        <button className="submit" onClick={onClickHandler}>SUBMIT</button>
+      </div>
     </div>
   );
 }
